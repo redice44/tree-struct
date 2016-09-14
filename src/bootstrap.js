2b63e84bbcb4758f65221fcacb9285c5a10561c1
Matt Thomson <red.cataclysm@gmail.com>
2016-09-14 09:20:52 -0400

    100% coverage on tree.js

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,3 +3,4 @@ import Tree from './tree.js';
 let tree = new Tree('Root');
 
 console.log(tree);
+console.log(tree.depthFirstPreOrder());
--- src/tree.js
+++ src/tree.js
@@ -2,8 +2,40 @@ import Node from './node';
 
 class Tree {
   constructor(rootData) {
-    console.log('Creating a new tree.');
     this._root = new Node(rootData);
+  }
+
+  depthFirstPreOrder() {
+    return this._depthFirstPreOrder(this._root);
+  }
+
+  depthFirstPreOrderFlat() {
+    return this._depthFirstPreOrderFlat(this._root);
+  }
+
+  _depthFirstPreOrderFlat(node) {
+    let n = [];
+    n.push(node.data);
+    if (node.children.length > 0) {
+      for (let c of node.children) {
+        n = n.concat(this._depthFirstPreOrderFlat(c));
+      }
+    }
+
+    return n;
+  }
+
+  _depthFirstPreOrder(node) {
+    let n = {};
+    n.value = node.data;
+    if (node.children.length > 0) {
+      n.children = [];
+
+      for (let c of node.children) {
+        n.children.push(this._depthFirstPreOrder(c));
+      }
+    }
+    return n;
   }
 
   /* Getters */
